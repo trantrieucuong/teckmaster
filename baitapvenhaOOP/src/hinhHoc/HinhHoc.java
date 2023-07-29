128ccd632368c8cc3470df05bbb28851c4888426
trantrieucuong <trancuong15112004@gmail.com>
2023-07-29 11:47:37 +0700

    Revert "áef"
    This reverts commit b292b60bbb7ced6016bc985e1f0b5a2068b7bbee.

---
+++ baitapvenhaOOP/src/hinhHoc/HinhHoc.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hinhHoc;
+
+/**
+ *
+ * @author Legion 5
+ */
+public class HinhHoc {
+    
+    // Phương thức tính diện tích (hình chữ nhật và hình vuông sẽ được cài đặt riêng)
+    public double tinhDienTich() {
+        return 0.0;
+    }
+
+    // Phương thức tính chu vi (hình chữ nhật và hình vuông sẽ được cài đặt riêng)
+    public double tinhChuVi() {
+        return 0.0;
+    }
+
+    // Phương thức hiển thị thông tin về các cạnh của hình (hình chữ nhật và hình vuông sẽ được cài đặt riêng)
+    public void hienThiThongTin() {
+    }
+}
+
